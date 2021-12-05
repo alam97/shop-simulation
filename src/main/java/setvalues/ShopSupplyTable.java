3c084aaf958151066f9682b3be301979bdc28b81
alam97 <arianalamar@gmail.com>
2021-12-05 18:37:21 +0100

    class to specify each shop's supplied ammount

---
+++ src/main/java/setvalues/ShopSupplyTable.java
@@ -0,0 +1,22 @@
+package setvalues;
+
+import java.util.Hashtable;
+import java.util.List;
+
+public class ShopSupplyTable {
+
+    private List<Integer> amounts;
+    // for each Shop, we have different supply policies
+
+    public ShopSupplyTable(List<Integer> amounts) {
+        this.amounts = amounts;
+    }
+
+    public Hashtable<Integer, Integer> getSupplyPolicy() {
+        Hashtable<Integer, Integer> supplyChain = new Hashtable<>();
+        for (int i = 0; i < amounts.size(); i++){
+            supplyChain.put(i, amounts.get(i));
+        }
+        return supplyChain;
+    }
+}
