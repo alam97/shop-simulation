820ead75637a1222875488a6002324512062eefc
alam97 <arianalamar@gmail.com>
2021-12-21 20:23:12 +0100

    shop implementing runnable for threading purposes: TEST

--- src/main/java/model/Shop.java
+++ src/main/java/model/Shop.java
@@ -1,12 +1,14 @@
 package model;
 import services.OrderHandler;
 import setvalues.ProductTable;
-import setvalues.ShopSupplyTable;
+import simulation.Sim;
 
 import java.util.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class Shop {
+public class Shop implements Runnable{
 
     public static final double MARKUP = 0.2;
     public static final double POLICY_MARKUP = 0.002;
@@ -16,14 +18,26 @@ public class Shop {
     private ProductTable productTable;
     private Warehouse warehouse;
     private OrderHandler orderHandler;
+    private Backlog backlog;
     private int id;
+
+    private Thread thread;
+    private BlockingQueue<Order> orderQueue = new LinkedBlockingQueue<>();
 
     public Shop(ProductTable productTable, List<Integer> amounts) {
         this.productTable = productTable;
         warehouse = new Warehouse(createCatalog(),amounts);
         orderHandler = new OrderHandler(warehouse);
         id = counter.incrementAndGet();
+        backlog = new Backlog();
+        thread = new Thread(this);
     }
+
+    public void startHandlingOrders(){
+        thread.start();
+    }
+
+
     private List<Product> createCatalog(){
        List<Product> catalog = new ArrayList<>();
         Hashtable<Integer, Double> priceTable = productTable.getProductTable();
@@ -40,12 +54,27 @@ public class Shop {
     public List<Product> getCatalog() {
         return warehouse.getInventory();
     }
-    public void handleOrder(Order order) { orderHandler.handleOrder(order);}
+
+    public void queueOrder(Order order){
+        try {
+            orderQueue.put(order);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleOrder(Order order) {
+        orderHandler.handleOrder(order);
+    }
 
     public int getId() { return id; }
 
     public void supplyShop(){
         warehouse.supplyWarehouse();
+    }
+
+    public Backlog getBacklog() {
+        return backlog;
     }
 
     @Override
@@ -54,6 +83,20 @@ public class Shop {
                 "productTable=" + productTable +
                 ", warehouse=" + warehouse +
                 ", orderHandler=" + orderHandler +
+                ", backlog=" + backlog +
+                ", id=" + id +
                 '}';
+    }
+
+    @Override
+    public void run() {
+        while (true){ //wykonywac dla wartosci day mniejszej niz dzien po ostatnim dniu symulacji. Nie mozna robić tak zeby take wywołano gdy juz nie bedzie wysylanych kolejnych orderow
+            try {
+                Order order = orderQueue.take();
+                handleOrder(order);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
