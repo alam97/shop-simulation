2e9ea59cd0582ffe97dce81630931834073aafb3
alam97 <arianalamar@gmail.com>
2021-12-21 20:22:22 +0100

    changed to queue

--- src/main/java/services/OrderHandler.java
+++ src/main/java/services/OrderHandler.java
@@ -4,15 +4,14 @@ import model.Order;
 import model.Product;
 import model.Shop;
 import model.Warehouse;
+import simulation.Sim;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class OrderHandler {
 
-    private List<Order> orderqueue = new ArrayList<>();
-    private List<Order> processedorders = new ArrayList<>();
+    private Queue<Order> orderqueue = new LinkedList<>();
+    private Queue<Order> processedorders = new LinkedList<>();
     private Warehouse warehouse;
     private Double profit = 0d;
     Policy policy = new Policy();
@@ -29,20 +28,25 @@ public class OrderHandler {
         }
 
         warehouse.updateInventory(product);
-        orderqueue.removeIf((o) -> orderqueue.contains(o));
-        order.setCompletionStatus(true);
-        checkForPolicy(product);
-        this.profit += product.getAmount() * product.getPrice();
+        order.setDayOfCompletion(Sim.day);
+        checkForPolicy(product); //ustawia promocje w produkcie
+        this.profit += product.getTotalPrice();
         this.processedorders.add(order);
-
     }
+
+    public void handleOldOrders(){
+        List<Order> oldOrders = new ArrayList<>(orderqueue);
+        orderqueue.clear();
+        oldOrders.forEach(o -> handleOrder(o));
+    }
+
 
     public Double getProfit() {
         return profit;
     }
 
     public void checkForPolicy(Product product) {
-        if (policy.ifPolicy(warehouse.getInventory().get(product.getId()).getAmount(), warehouse.getSuppliedAmount(product.getId()))) {
+        if (policy.ifPolicy(warehouse.getInventory().get(product.getId()-1).getAmount(), warehouse.getSuppliedAmount(product.getId()-1))) {
             product.setPricewithMarkup(Shop.POLICY_MARKUP);
         } else {
             product.setPricewithMarkup(Shop.MARKUP);
@@ -50,11 +54,11 @@ public class OrderHandler {
     }
 
     public List<Order> getOrderqueue() {
-        return Collections.unmodifiableList(orderqueue);
+        return Collections.unmodifiableList(new ArrayList<>(orderqueue));
     }
 
     public List<Order> getProcessedorders() {
-        return Collections.unmodifiableList(processedorders);
+        return Collections.unmodifiableList(new ArrayList<>(processedorders));
     }
 
         @Override
