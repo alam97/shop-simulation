7503ca3abeb9430774e12d5289c6930f279ce69a
alam97 <arianalamar@gmail.com>
2021-12-22 16:58:20 +0100

    class for threading each shop

--- src/main/java/simulation/SimShop.java
+++ src/main/java/simulation/SimShop.java
@@ -6,31 +6,85 @@ import model.Product;
 import model.Shop;
 import services.AmountChoice;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
-public class SimShop {
+public class SimShop implements Runnable{
 
     private Shop shop;
     private List<Client> clients;
     private List<Order> orders = new LinkedList<>();
+    private BlockingQueue<Order> orderQueue = new LinkedBlockingQueue<>();
+    private Thread thread;
 
-    // na poczatku kazdy sklep dostaje swoja liste klientow
     public SimShop(Shop shop, List<Client> clients) {
         this.shop = shop;
         this.clients = clients;
+        thread = new Thread(this);
+    }
+
+    public void startHandlingOrders(){
+        thread.start();
+    }
+
+    public void stopSimulation() { thread.interrupt();}
+
+    public void queueOrder(Order order){
+    //    System.out.println("======== " + order);
+        try {
+            orderQueue.put(order);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+     //   System.out.println(orderQueue);
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public Shop getShop() {
+        return shop;
+    }
+
+    public BlockingQueue<Order> getOrderQueue() {
+        return orderQueue;
+    }
+
+    @Override
+    public void run() {
+        while (Sim.day < Sim.finalday){
+            try {
+                Order order = orderQueue.take();
+                shop.handleOrder(order);
+                System.out.println(order);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "SimShop{" +
+                "shop=" + shop +
+                ", clients=" + clients +
+                ", orders=" + shop.getOrderHandler() +
+                ", orderQueue=" + orderQueue +
+                '}';
+    }
+
+    /*    public void newClients(List<Client> c){
+        clients.clear();
+        clients.addAll(c);
     }
 
     // clients make orders
     private void clientsMakeOrders(){
         AmountChoice amountChoice = new AmountChoice();
         clients.forEach((c) -> orders.add(new Order(shop, c, Sim.getDayofmonth())));
-    }
-    // handle orders
-//    public void handleOrders(){
-//        orders.stream().filter( o -> !o.isComplete()).forEach( o -> shop.handleOrder(o));
-//    }
-
-    // giverating
+    }*/
 }
